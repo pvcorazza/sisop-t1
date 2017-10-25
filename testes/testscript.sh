5d258027ebd7fb94be612f44adcf0a949ef43ac9
marcelojw8 <marcelojw8@gmail.com>
2017-10-25 12:48:08 -0200

    Delete testscript.sh

--- testes/testscript.sh
+++
@@ -1,9 +0,0 @@
-# Script para testar um teste com a biblioteca cthread criada
-
-gcc -Wall testDeadLock.c -c
-gcc -Wall ../src/cthread.c -c
-gcc testDeadLock.o cthread.o ../bin/support.o -o Teste
-./Teste
-echo " "
-rm *.o
-rm Teste
